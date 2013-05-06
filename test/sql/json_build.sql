090463d9aa0e85d4e18d063398fc63f4058dd8fa
Andrew Dunstan <andrew@dunslane.net>
2013-05-06 10:59:25 -0400

    add more regression tests

--- test/sql/json_build.sql
+++ test/sql/json_build.sql
@@ -8,3 +8,21 @@ SELECT build_json_object(
        'd', build_json_object('e',array[9,8,7]::int[],
            'f', (select row_to_json(r) from ( select relkind, oid::regclass as name from pg_class where relname = 'pg_class') r)));
 
+
+-- empty objects/arrays
+SELECT build_json_array();
+
+SELECT build_json_object();
+
+-- make sure keys are quoted
+SELECT build_json_object(1,2);
+
+-- keys must be scalar and not null
+SELECT build_json_object(null,2);
+
+SELECT build_json_object(r,2) FROM (SELECT 1 AS a, 2 AS b) r;
+
+SELECT build_json_object(json '{"a":1,"b":2}', 3);
+
+SELECT build_json_object('{1,2,3}'::int[], 3);
+
