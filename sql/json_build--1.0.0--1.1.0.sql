eb27d22d1603c4f6743441ded6a6ef9a7b0a1ae9
Andrew Dunstan <andrew@dunslane.net>
2013-10-05 16:51:48 -0400

    fix migration script

--- sql/json_build--1.0.0--1.1.0.sql
+++ sql/json_build--1.0.0--1.1.0.sql
@@ -2,7 +2,7 @@
 \echo Use "ALTER EXTENSION json_build UPDATE TO '1.1.0'" to load this file. \quit
 
 
-CREATE FUNCTION json_object_agg_transfn(internal, text, anyelement)
+CREATE FUNCTION json_object_agg_transfn(internal, "any", "any")
 RETURNS internal
 AS 'MODULE_PATHNAME'
 LANGUAGE C IMMUTABLE;
@@ -12,7 +12,7 @@ RETURNS json
 AS 'MODULE_PATHNAME'
 LANGUAGE C IMMUTABLE;
 
-CREATE AGGREGATE json_object_agg(text, anyelement) (
+CREATE AGGREGATE json_object_agg("any", "any") (
     SFUNC = json_object_agg_transfn,
     FINALFUNC = json_object_agg_finalfn,
     STYPE = internal
